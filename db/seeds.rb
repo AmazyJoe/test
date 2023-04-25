bfce18cd69566dcc3857a9553a98005e514cf1fd
AmazyJoe <ochiengolu@gmail.com>
2023-04-25 12:47:13 +0300

    more changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,6 +45,8 @@ def create_product_entries(name, price, delivery_cost, mode, image_url, website_
 end
 
 
+
+
 create_product_entries("Samsung Galaxy S21", 269.99, 50.00, "Fast Shipping", "https://m.media-amazon.com/images/I/61jYjeuNUnL.__AC_SX300_SY300_QL70_FMwebp_.jpg", "https://www.amazon.com/Samsung-Galaxy-S21-5G-Version/dp/B08VLMQ3KS/ref=sr_1_2?crid=U39RSMTSMJP6&keywords=samsung+galaxy+s21&qid=1682071708&sprefix=samsung+galaxy+s21%2Caps%2C347&sr=8-2", "amazon")
 create_product_entries("Samsung Galaxy S21", 360.99, 50.00, "Fast Shipping", "https://s.alicdn.com/@sc04/kf/H8ffaafcbc30f4439b5ec8f7f346fb9a1i.jpg_300x300.jpg", "https://www.alibaba.com/trade/search?tab=all&searchText=samsung+galaxy+s21", "alibaba")
 create_product_entries("Samsung Galaxy S21", 234.99, 50.00, "Fast Shipping", "https://i.ebayimg.com/images/g/UaQAAOSwwEVjVtkE/s-l500.jpg", "https://www.ebay.com/itm/255798953595?epid=28043687665&hash=item3b8ece467b:g:I1AAAOSwPhljW9x~&amdata=enc%3AAQAIAAAA4GgTB4H%2FZK6MMdgpkLg1mViaGC7J6MM1utmNO%2BYPKtNWcVWfYSTkiaqsOfeDj6faWqdFxtGzzH8G8l0T7vPQNb3nA4roNAOmxQPRshnb3FVrICdyLvJW77xrJBLGcLN4i6thrKYR%2FfWXHONk2TiUMMKzL0FWmGRhyQWIYptzGAEyBKlOSOgznPrdMooGxWES2jVnkLLxedJ1hE8qtv340zBL3rupIoXC1ALpEB2nrxah4phrGRI8%2BIo5ZvK2id210oVrQ07x6KNS%2BIp4FH95rJmUiN5N1y0suoPx2o2q8tNZ%7Ctkp%3ABFBMxveYtvRh&var=555735987575", "ebay")
@@ -96,4 +98,19 @@ create_product_entries("jbl flip 6", 79.99, 90.00, "Fast Shipping", "https://i.e
 create_product_entries("Nikon D3500", 719.99, 40.00, "Fast Shipping", "https://m.media-amazon.com/images/I/61s8FKpmqML._AC_UY218_.jpg", "https://www.amazon.com/s?k=nikon+camera&crid=MLOOMRS38GO6&sprefix=nikon+cam%2Caps%2C428&ref=nb_sb_ss_ts-doa-p_2_9", "amazon")
 create_product_entries("Nikon D3500", 450.99, 20.00, "Fast Shipping", "https://s.alicdn.com/@sc04/kf/H4b2c1b40f1374024a89a17ed233216c2v.jpg_300x300.jpg", "https://www.alibaba.com/trade/search?tab=all&searchText=Nikon+D3500+camra", "alibaba")
 create_product_entries("Nikon D3500", 535.99, 70.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/qu4AAOSwbLRj6UAH/s-l300.webp", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=NIKON+D3500&_sacat=0&LH_TitleDesc=0&_odkw=jbl+flip6&_osacat=0", "ebay")
+
+
+
+
+#name, price, delivery_cost, mode, image_url, website_url, website_name
+
+create_product_entries("Samsung", 450.99, 20.00, "Fast Shipping", "https://s.alicdn.com/@sc04/kf/H4b2c1b40f1374024a89a17ed233216c2v.jpg_300x300.jpg", "https://www.alibaba.com/trade/search?tab=all&searchText=Nikon+D3500+camra", "amazon")
+create_product_entries("Samsung", 535.99, 70.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/qu4AAOSwbLRj6UAH/s-l300.webp", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=NIKON+D3500&_sacat=0&LH_TitleDesc=0&_odkw=jbl+flip6&_osacat=0", "alibaba")
+create_product_entries("Samsung", 589.99, 40.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/rZUAAOSwZcBjo3a-/s-l225.webp", "https://www.ebay.com/itm/255890067629?epid=27052277424&hash=item3b943c90ad:g:rZUAAOSwZcBjo3a-&amdata=enc%3AAQAIAAAA4Dd1y62Rz5tJ3pcAtLqdYZi4LT8OsP0ap%2BwiTbnHPQ5Gqqw%2FRpy9Daj7KcHp%2BYlLTAivMyuWNQuZF%2Bjb3qiGZY1yycLTbUNRt1qDXza%2FJ1vFGs5xcjL4sE7U4UedijBNj%2FkGxV4xj%2F3YhLqEzcWs%2FpFoLEtw5ec5EehR7fkVgK1AGNAAKlx1GcaLT6LObm9bww0sTFnK%2FdVO%2BUevICJFucBzsjjmJml%2FAsgJM5aVRL%2B7RuSnaelZ7SELb%2BQRCh7t%2F%2FJKVJ9nP%2FvWvD5kPdYqnlc4a1E1%2BtK7%2FVSkoO9m7Mxg%7Ctkp%3ABFBMiKmm-_Zh", "ebay")
+create_product_entries("LG", 719.99, 40.00, "Fast Shipping", "https://m.media-amazon.com/images/I/61s8FKpmqML._AC_UY218_.jpg", "https://www.amazon.com/s?k=nikon+camera&crid=MLOOMRS38GO6&sprefix=nikon+cam%2Caps%2C428&ref=nb_sb_ss_ts-doa-p_2_9", "amazon")
+create_product_entries("LG", 450.99, 20.00, "Fast Shipping", "https://s.alicdn.com/@sc04/kf/H4b2c1b40f1374024a89a17ed233216c2v.jpg_300x300.jpg", "https://www.alibaba.com/trade/search?tab=all&searchText=Nikon+D3500+camra", "alibaba")
+create_product_entries("LG", 535.99, 70.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/qu4AAOSwbLRj6UAH/s-l300.webp", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=NIKON+D3500&_sacat=0&LH_TitleDesc=0&_odkw=jbl+flip6&_osacat=0", "ebay")
+create_product_entries("Ramtons", 450.99, 20.00, "Fast Shipping", "https://s.alicdn.com/@sc04/kf/H4b2c1b40f1374024a89a17ed233216c2v.jpg_300x300.jpg", "https://www.alibaba.com/trade/search?tab=all&searchText=Nikon+D3500+camra", "amazon")
+create_product_entries("Ramtons", 535.99, 70.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/qu4AAOSwbLRj6UAH/s-l300.webp", "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=NIKON+D3500&_sacat=0&LH_TitleDesc=0&_odkw=jbl+flip6&_osacat=0", "alibaba")
+create_product_entries("Ramtons", 589.99, 40.00, "Fast Shipping", "https://i.ebayimg.com/thumbs/images/g/rZUAAOSwZcBjo3a-/s-l225.webp", "https://www.ebay.com/itm/255890067629?epid=27052277424&hash=item3b943c90ad:g:rZUAAOSwZcBjo3a-&amdata=enc%3AAQAIAAAA4Dd1y62Rz5tJ3pcAtLqdYZi4LT8OsP0ap%2BwiTbnHPQ5Gqqw%2FRpy9Daj7KcHp%2BYlLTAivMyuWNQuZF%2Bjb3qiGZY1yycLTbUNRt1qDXza%2FJ1vFGs5xcjL4sE7U4UedijBNj%2FkGxV4xj%2F3YhLqEzcWs%2FpFoLEtw5ec5EehR7fkVgK1AGNAAKlx1GcaLT6LObm9bww0sTFnK%2FdVO%2BUevICJFucBzsjjmJml%2FAsgJM5aVRL%2B7RuSnaelZ7SELb%2BQRCh7t%2F%2FJKVJ9nP%2FvWvD5kPdYqnlc4a1E1%2BtK7%2FVSkoO9m7Mxg%7Ctkp%3ABFBMiKmm-_Zh", "amazon")
 
